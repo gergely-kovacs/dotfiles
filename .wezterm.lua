956c9b3e58db6486914b91f688332d58e2d12969
Gergely Kovacs <gergely-kovacs@hotmail.com>
2024-06-04 23:01:42 +0200

    removed wezterm ligatures

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,6 +6,7 @@ local config = wezterm.config_builder()
 config.color_scheme = "ayu"
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.window_padding = {
 	left = 0,
 	right = 0,
