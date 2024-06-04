3069db74e9533120ee6323789d5e2e49dc603cca
Gergely Kovacs <gergely-kovacs@hotmail.com>
2024-06-04 22:59:11 +0200

    removed padding from wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,15 +1,21 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local mux = wezterm.mux
 local config = wezterm.config_builder()
 
-config.color_scheme = 'ayu'
+config.color_scheme = "ayu"
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
 
-wezterm.on('gui-startup', function(cmd)
-  local tab, pane, window = mux.spawn_window(cmd or {})
-  window:gui_window():maximize()
+wezterm.on("gui-startup", function(cmd)
+	local _, _, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
 end)
 
 return config
