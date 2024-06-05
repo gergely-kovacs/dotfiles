b36a665c74fa66708ab8a5c693d7ec357541490f
Gergely Kovacs <gergely-kovacs@hotmail.com>
2024-06-06 00:27:58 +0200

    added smart-splits, moved wezterm conf

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,44 @@
+local wezterm = require 'wezterm'
+local smart_splits = wezterm.plugin.require 'https://github.com/mrjones2014/smart-splits.nvim'
+
+local mux = wezterm.mux
+local config = wezterm.config_builder()
+
+config.color_scheme = 'ayu'
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
+config.leader = { key = 's', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.keys = {
+  {
+    key = '=',
+    mods = 'LEADER',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = '-',
+    mods = 'LEADER',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 's',
+    mods = 'LEADER|CTRL',
+    action = wezterm.action.SendKey { key = 's', mods = 'CTRL' },
+  },
+}
+
+smart_splits.apply_to_config(config)
+
+wezterm.on('gui-startup', function(cmd)
+  local _, _, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
+
+return config
