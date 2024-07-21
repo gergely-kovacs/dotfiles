ede3346e68249aa9c5d912f0db7419d6cdca0d65
Gergely Kovacs <gergely-kovacs@hotmail.com>
2024-07-21 19:03:20 +0200

    changed wezterm color scheme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,7 +4,7 @@ local smart_splits = wezterm.plugin.require 'https://github.com/mrjones2014/smar
 local mux = wezterm.mux
 local config = wezterm.config_builder()
 
-config.color_scheme = 'ayu'
+config.colors = require 'cyberdream'
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
