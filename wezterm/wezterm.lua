47fade77c1fe7699223a4132a4626e6312647ff0
Gergely Kovacs <gergely-kovacs@hotmail.com>
2024-09-08 09:28:39 +0200

    added resurrect plugin to wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 local wezterm = require 'wezterm'
 local smart_splits = wezterm.plugin.require 'https://github.com/mrjones2014/smart-splits.nvim'
+local resurrect = wezterm.plugin.require 'https://github.com/MLFlexer/resurrect.wezterm'
 
 local mux = wezterm.mux
 local config = wezterm.config_builder()
@@ -8,6 +9,8 @@ config.colors = require 'cyberdream'
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+config.font_size = 12
+config.font = wezterm.font 'FiraMono Nerd Font'
 config.window_padding = {
   left = 0,
   right = 0,
@@ -31,6 +34,43 @@ config.keys = {
     key = 'm',
     mods = 'LEADER',
     action = wezterm.action.TogglePaneZoomState,
+  },
+  {
+    key = 's',
+    mods = 'LEADER',
+    action = wezterm.action_callback(function(win, pane)
+      resurrect.save_state(resurrect.workspace_state.get_workspace_state())
+      resurrect.window_state.save_window_action()
+    end),
+  },
+  {
+    key = 'l',
+    mods = 'LEADER',
+    action = wezterm.action_callback(function(win, pane)
+      resurrect.fuzzy_load(win, pane, function(id, label)
+        local type = string.match(id, '^([^/]+)') -- match before '/'
+        id = string.match(id, '([^/]+)$') -- match after '/'
+        id = string.match(id, '(.+)%..+$') -- remove file extension
+        local state
+        if type == 'workspace' then
+          state = resurrect.load_state(id, 'workspace')
+          resurrect.workspace_state.restore_workspace(state, {
+            relative = true,
+            restore_text = true,
+            on_pane_restore = resurrect.tab_state.default_on_pane_restore,
+          })
+        elseif type == 'window' then
+          state = resurrect.load_state(id, 'window')
+          resurrect.window_state.restore_window(pane:window(), state, {
+            relative = true,
+            restore_text = true,
+            on_pane_restore = resurrect.tab_state.default_on_pane_restore,
+            -- uncomment this line to use active tab when restoring
+            -- tab = win:active_tab(),
+          })
+        end
+      end)
+    end),
   },
   {
     key = 's',
