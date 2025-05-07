5e9c5c5a5ed4786b644bdd9067722bf7c244cc9b
Gergely Kovacs <gergely-kovacs@hotmail.com>
2025-05-07 22:29:36 +0200

    enabled borders globally

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -78,9 +78,5 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
--- Diagnostics
-vim.diagnostic.config {
-  float = {
-    border = 'rounded',
-  },
-}
+-- Window border
+vim.opt.winborder = 'single'
