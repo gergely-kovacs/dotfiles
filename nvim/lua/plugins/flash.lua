499d6197f87e21871685a0435d135a47c5672d95
Gergely Kovacs <gergely-kovacs@hotmail.com>
2025-05-11 11:46:15 +0200

    Ignored missing fields in flash config, removed stylua ignore

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -2,13 +2,48 @@ return {
   'folke/flash.nvim',
   event = 'BufReadPost',
   ---@type Flash.Config
+  ---@diagnostic disable-next-line: missing-fields
   opts = {},
-  -- stylua: ignore
   keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<C-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    {
+      's',
+      mode = { 'n', 'x', 'o' },
+      function()
+        require('flash').jump()
+      end,
+      desc = 'Flash',
+    },
+    {
+      'S',
+      mode = { 'n', 'o', 'x' },
+      function()
+        require('flash').treesitter()
+      end,
+      desc = 'Flash Treesitter',
+    },
+    {
+      'r',
+      mode = 'o',
+      function()
+        require('flash').remote()
+      end,
+      desc = 'Remote Flash',
+    },
+    {
+      'R',
+      mode = { 'o', 'x' },
+      function()
+        require('flash').treesitter_search()
+      end,
+      desc = 'Treesitter Search',
+    },
+    {
+      '<C-s>',
+      mode = { 'c' },
+      function()
+        require('flash').toggle()
+      end,
+      desc = 'Toggle Flash Search',
+    },
   },
 }
