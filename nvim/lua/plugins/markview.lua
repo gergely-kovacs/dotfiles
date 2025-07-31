a2e36e7743116a8df5883a2515838eadf8d77dfc
Gergely Kovacs <gergely.kovacs@hotmail.com>
2025-07-31 06:43:43 +0200

    disabled markview hybrid mode

--- nvim/lua/plugins/markview.lua
+++ nvim/lua/plugins/markview.lua
@@ -1,16 +1,10 @@
 return {
   'OXY2DEV/markview.nvim',
   lazy = false,
-  -- ft = "markdown" -- If you decide to lazy-load anyway
   dependencies = {
     'nvim-treesitter/nvim-treesitter',
     'nvim-tree/nvim-web-devicons',
   },
-  opts = {
-    preview = {
-      icon_provider = 'devicons',
-      hybrid_modes = { 'n' },
-      modes = { 'n', 'no', 'c' },
-    },
-  },
+  ---@class mkv.config
+  opts = {},
 }
