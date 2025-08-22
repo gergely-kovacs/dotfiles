fbd65fe2b5bd3622d7e47fa0b01d30fa34b546a6
Gergely Kovacs <gergely.kovacs@hotmail.com>
2025-08-22 18:26:44 +0200

    improved lualine styling

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -19,7 +19,51 @@ return {
       end
     end
 
+    -- https://github.com/scottmckendry/cyberdream.nvim/blob/main/lua/cyberdream/colors.lua
+    local colors = {
+      bg = '#16181a',
+      bg_alt = '#1e2124',
+      bg_highlight = '#3c4048',
+      fg = '#ffffff',
+      grey = '#7b8496',
+      blue = '#5ea1ff',
+      green = '#5eff6c',
+      cyan = '#5ef1ff',
+      red = '#ff6e5e',
+      yellow = '#f1ff5e',
+      magenta = '#ff5ef1',
+      pink = '#ff5ea0',
+      orange = '#ffbd5e',
+      purple = '#bd5eff',
+    }
+
+    -- https://github.com/scottmckendry/cyberdream.nvim/blob/main/lua/cyberdream/extensions/heirline.lua
+    local lualine_theme = {
+      inactive = {
+        a = { fg = colors.fg, bg = colors.bg },
+        b = { fg = colors.fg, bg = colors.bg },
+        c = { fg = colors.fg, bg = colors.bg },
+      },
+      normal = {
+        a = { fg = colors.bg, bg = colors.blue },
+        b = { fg = colors.bg, bg = colors.grey },
+        c = { fg = colors.fg, bg = colors.bg },
+      },
+      visual = { a = { fg = colors.bg, bg = colors.magenta } },
+      replace = { a = { fg = colors.bg, bg = colors.red } },
+      command = { a = { fg = colors.bg, bg = colors.yellow } },
+      insert = { a = { fg = colors.bg, bg = colors.green } },
+      terminal = { a = { fg = colors.bg, bg = colors.cyan } },
+    }
+
+    -- https://github.com/ryanoasis/powerline-extra-symbols
+    -- https://www.nerdfonts.com/cheat-sheet
     require('lualine').setup {
+      options = {
+        theme = lualine_theme,
+        component_separators = '',
+        section_separators = { left = ' ', right = ' ' },
+      },
       sections = {
         lualine_a = {
           'mode',
