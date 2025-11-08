f691c5c3fe93b398abf8dd6388cd0e54a712cece
Gergely Kovacs <gergely-kovacs@hotmail.com>
2025-11-08 10:52:12 +0100

    Removed godothost because it caused errors with multiple instances

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -84,10 +84,3 @@ vim.diagnostic.config {
     border = 'rounded',
   },
 }
-
--- Autostart godothost
-local gdproject = io.open(vim.fn.getcwd() .. '/project.godot', 'r')
-if gdproject then
-  io.close(gdproject)
-  vim.fn.serverstart './godothost'
-end
